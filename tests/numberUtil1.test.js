cf0e8d1c6e624560ccee17eeed70cee7ea7bc881
Varun Kumar <varu1311@gmail.com>
2023-01-15 17:53:59 +0530

    chore: test numberUtil1

---
+++ tests/numberUtil1.test.js
@@ -0,0 +1,14 @@
+const areDigitSameArrow = require("../numberUtil1");
+const areDigitSame = require("../numberUtil1");
+
+const cases = [
+    [222, true],
+    [123, false],
+    [1, true]
+];
+
+describe("Are digits same (regular function)", () => {
+    test.each(cases)("%d should give %s", (input, expected) => {
+        expect(areDigitSame(input)).toBe(expected);
+    });
+});
