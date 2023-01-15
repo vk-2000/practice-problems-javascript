d64075ccc8bd2ce34c5995dcf952aae40c6329ae
Varun Kumar <varu1311@gmail.com>
2023-01-15 17:09:38 +0530

    feat: add functionality to count vowels in a string

---
+++ stringUtil4.js
@@ -0,0 +1,37 @@
+function countVowels(str) {
+    let vowels = 0;
+    for(let i=0;i<str.length;i++){
+        switch (str.charAt(i).toUpperCase()) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U': 
+                vowels++;
+                break;
+            default:
+                break;
+        }
+    }
+    return vowels;
+}
+
+const countVowelsArrow = str => {
+    let vowels = 0;
+    for(let i=0;i<str.length;i++){
+        switch (str.charAt(i).toUpperCase()) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U': 
+                vowels++;
+                break;
+            default:
+                break;
+        }
+    }
+    return vowels;
+}
+
+console.log(countVowels("codeacademy"));
