6a62e738eedaaeb4dc02bca758fc5321d7a44cb7
Varun Kumar <varu1311@gmail.com>
2023-01-15 17:06:33 +0530

    feat: add functionality to check for leap year

---
+++ numberUtil2.js
@@ -0,0 +1,7 @@
+function isLeapYear(year){
+    return year % 400 == 0 ? true : ((year % 4 == 0) && (year % 100 != 0) ? true : false)
+}
+
+const isLeapYearArrow = year => year % 400 == 0 ? true : ((year % 4 == 0) && (year % 100 != 0) ? true : false)
+
+console.log(isLeapYearArrow(2001));
