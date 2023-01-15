757178bac869063a9160a9b68d8525be3af51fd3
Varun Kumar <varu1311@gmail.com>
2023-01-15 17:08:43 +0530

    feat: add functionality to concat two strings

---
+++ stringUtil3.js
@@ -0,0 +1,9 @@
+function concatStrings(array) {
+    let result = array[0].slice(1).concat(array[1].slice(1));
+    return result;
+}
+
+const concatStringsArrow = array => array[0].slice(1).concat(array[1].slice(1));
+
+
+console.log(concatStringsArrow(['code', 'academy']));
