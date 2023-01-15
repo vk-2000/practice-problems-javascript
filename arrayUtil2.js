a85fb448897ae2e3c31e2cdd6b434b226ecedb47
Varun Kumar <varu1311@gmail.com>
2023-01-15 17:05:29 +0530

    feat: add functionality to find longest string in an array

---
+++ arrayUtil2.js
@@ -0,0 +1,8 @@
+function longestString(array){
+    let longest = array[0];
+    return array.reduce((longest, current) => current.length > longest.length ? current : longest, longest)
+}
+
+const longestStringArrow = (array) => array.reduce((longest, current) => current.length > longest.length ? current : longest, array[0])
+
+console.log(longestStringArrow(["", "love", "code code code", "academy"]));
